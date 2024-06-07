8e710d72ba869781f37eee09d34a705b313300bd
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-06-07 17:54:50 +0200

    [ADD] voto espresso in stelle

--- src/components/CardFilmComponent.vue
+++ src/components/CardFilmComponent.vue
@@ -13,64 +13,51 @@ export default {
             bandieraItalia: "src/assets/img/bandiera-italia.png",
             bandierauk: "src/assets/img/bandiera-uk.webp",
             imgBaseUrl: "https://image.tmdb.org/t/p/w342",
-            votoDecimi: this.voto,
+            votoDecimi: this.voto
         };
     },
-    methods: {
-        // converto il voto da 10 a max 5
+    computed: {
         nuovoVoto() {
             const votoDimezzato = this.votoDecimi / 2;
             return Math.ceil(votoDimezzato);
         },
-        // aggiungo le stelle vuote
         stelleVuote() {
-            if (this.nuovoVoto() < 5) {
-                const mancanti = 5 - this.nuovoVoto();
-                return mancanti;
-            }
-        },
-
-    },
+            return 5 - this.nuovoVoto;
+        }
+    }
 };
 </script>
 
 <template>
-    <!-- copertina -->
     <div>
-        <img v-if="imgBaseUrl + copertina !== 'https://image.tmdb.org/t/p/w342null'" :src="imgBaseUrl + copertina" :alt="titolo">
-    </div>
-    <!-- copertina -->
-    <div>
+        <!-- copertina -->
         <div>
-            <h2>
-                {{ titolo }}
-            </h2>
+            <img v-if="imgBaseUrl + copertina !== 'https://image.tmdb.org/t/p/w342null'" :src="imgBaseUrl + copertina"
+                :alt="titolo">
         </div>
-
-        <div v-show="titolo !== titoloOriginale">
-            <h2>
-                {{ titoloOriginale }}
-            </h2>
+        <!-- titolo -->
+        <div>
+            <div>
+                <h2>{{ titolo }}</h2>
+            </div>
+            <div v-show="titolo !== titoloOriginale">
+                <h2>{{ titoloOriginale }}</h2>
+            </div>
         </div>
         <!-- lingua -->
         <div>
-            <!-- bandiera -->
-        <div>
-            <img v-if="lingua === 'it'" :src="bandieraItalia" alt="bandiera italiana">
-
-            <img v-else-if="lingua === 'en'" :src="bandierauk" alt="bandiera uk">
-
-            <h2 v-else>{{ lingua }}</h2>
-
-
+            <div>
+                <img v-if="lingua === 'it'" :src="bandieraItalia" alt="bandiera italiana">
+                <img v-else-if="lingua === 'en'" :src="bandierauk" alt="bandiera UK">
+                <h2 v-else>{{ lingua }}</h2>
+            </div>
         </div>
-        </div>
-        <!-- /lingua -->
-
         <!-- voto -->
         <div>
-            
+            <!-- stelle piene -->
+            <font-awesome-icon v-for="n in nuovoVoto" :key="'filled-' + n" :icon="['fas', 'star']" />
+            <!-- stelle vuote -->
+            <font-awesome-icon v-for="n in stelleVuote" :key="'empty-' + n" :icon="['far', 'star']" />
         </div>
     </div>
-    <!-- /voto -->
 </template>
