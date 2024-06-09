6a475d7000cc7e761b35f3336a7ed7286f75b629
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-06-09 10:12:28 +0200

    [FIX] eliminato codice inutile e aggiunto overview a cardfilmcomponent

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -21,29 +21,25 @@ export default {
 <template>
     <main class="p-3">
         <h2 class="m-5">{{ store.movies.titleMovie }}</h2>
+
         <!-- lista film -->
         <ul v-if="store.movies.filmCard.length > 0" class="d-flex flex-wrap justify-content-center gap-3 p-0">
             <li v-for="card in store.movies.filmCard" :key="card.id">
+
+                <!-- card -->
                 <CardFilmComponent
                     :copertina="card.poster_path"
                     :titolo="card.title"
                     :titoloOriginale="card.original_title"
                     :lingua="card.original_language"
                     :voto="card.vote_average"
+                    :overview="card.overview"
                 />
+                <!-- /card -->
+
             </li>
 
-            <li v-if="!store.movies.filmCard">
-            Caricamento in corso...
-            </li>
-            <li v-for="card in store.movies.filmCard || []" :key="card.id">
-            <CardFilmComponent
-                :titolo="card.title"
-                :titoloOriginale="card.original_title"
-                :lingua="card.original_language"
-                :voto="card.vote_average"
-            />
-            </li>
+
         </ul>
         <!-- lista film -->
 
@@ -52,14 +48,17 @@ export default {
         <h2 class="m-5">{{ store.tvShow.titleTv }}</h2>
         <ul v-if="store.tvShow.cardTv && store.tvShow.cardTv.length > 0" class="d-flex flex-wrap justify-content-center gap-3 p-0" >
             <li v-for="tvCard in store.tvShow.cardTv" :key="tvCard.id">
+                <!-- tv card -->
                 <CardFilmComponent 
                     :copertina="tvCard.poster_path"
                     :titolo="tvCard.name" 
                     :titoloOriginale="tvCard.original_name" 
                     :lingua="tvCard.original_language" 
                     :voto="tvCard.vote_average" 
+                    :overview="tvCard.overview"
                 />
             </li>
+            <!-- /tv card -->
         </ul>
         <!-- /lista tv -->
     </main>
